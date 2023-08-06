df9b7747c399668b81ccee09b82259f5414e277a
Diogo de Souza <di_smgomes@hotmail.com>
2023-08-06 19:01:03 -0300

    feat: creating user register req, and applied in sign up page

--- studyio/src/hooks/useUser.js
+++ studyio/src/hooks/useUser.js
@@ -5,3 +5,10 @@ export const LoginUser = (data) => {
     return axios.post(url, data)
     
 }
+
+export const CreateUser = (username, data) => {
+    var url = `http://192.168.100.5:8000/user/create/${username}    `
+    return axios.post(url, data)
+    
+}
+
