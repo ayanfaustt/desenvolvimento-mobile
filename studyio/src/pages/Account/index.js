2a4e0f8f38dee549c96ede449ad58def4d128066
Diogo de Souza <di_smgomes@hotmail.com>
2023-08-07 16:28:50 -0300

    feat: starting account page style

--- studyio/src/pages/Account/index.js
+++ studyio/src/pages/Account/index.js
@@ -6,21 +6,40 @@ export function Account() {
 
     return (
         <View style={globalStyles.container2}>
-            <Image
-                source={require('../../assets/cara.png')}
-                style={{alignSelf: 'center', marginTop: 30}}/>
-            <TextInput style={styles.banana}/>
-            <TextInput style={styles.banana}/>
-            <TouchableOpacity 
+            <View>
+                <Image
+                    source={require('../../assets/cara.png')}
+                    style={{ alignSelf: 'center', marginTop: 30 }} />
+                <Image
+                    source={require('../../assets/edit.png')}
+                    style={{ alignSelf: 'center', marginTop: 30 }} />
+            </View>
+            <View style={styles.fields}>
+                <View style={styles.fieldsInside}>
+                    <View style={styles.iconInput}>
+                        <Image
+                            source={require('../../assets/user-icon.png')}
+                            style={{}} />
+                        <TextInput style={styles.input} placeholder='user' />
+                    </View>
+                    <Image
+                        source={require('../../assets/edit-2.png')}
+                        style={{}} />
+                </View>
+            </View>
+            <View>
+                <TextInput style={styles.banana} />
+            </View>
+            <TouchableOpacity
                 style={styles.button}>
-                    <Text style={globalStyles.textButton}>Change password</Text>
+                <Text style={globalStyles.textButton}>Change password</Text>
             </TouchableOpacity>
         </View>
     )
 }
 
-const styles = StyleSheet.create ({
-    banana:{
+const styles = StyleSheet.create({
+    banana: {
         marginLeft: 30,
         marginRight: 30,
         marginTop: 80,
@@ -28,7 +47,7 @@ const styles = StyleSheet.create ({
         padding: 2,
         backgroundColor: '#A4C3DA',
     },
-    button:{
+    button: {
         justifyContent: 'center',
         backgroundColor: '#004257',
         borderRadius: 10,
@@ -38,5 +57,21 @@ const styles = StyleSheet.create ({
         alignItems: 'center',
         bottom: '10%',
         marginTop: 100
+    },
+    fields: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 28,
+    },
+    fieldsInside: {
+        flexDirection: 'row',
+        borderBottomWidth: 2,
+    },
+    input: {
+
+    },
+    iconInput: {
+        flexDirection: 'row',
     }
 })
